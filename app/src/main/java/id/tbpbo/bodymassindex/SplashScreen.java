1de415ee57be2a74dbd6acfc6fd5701855228d71
Deralvin <ramadhanrizqi21@gmail.com>
2021-01-31 15:37:58 +0700

    push

--- app/src/main/java/id/tbpbo/bodymassindex/SplashScreen.java
+++ app/src/main/java/id/tbpbo/bodymassindex/SplashScreen.java
@@ -1,15 +1,32 @@
 package id.tbpbo.bodymassindex;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 3000;  //2 Seconds
+    InternalStorage storage = new InternalStorage();
+    SharedPreferences mSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mSettings = getSharedPreferences("Storage", Context.MODE_PRIVATE);
+        if(mSettings.getString(Constant.name_shared,null) !=null && mSettings.getString(Constant.gender_shared,null) !=null
+        && mSettings.getString(Constant.umur_shared,null) !=null){
+            Intent i = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(i);
+        }else{
+            Intent i = new Intent(SplashScreen.this, NameActivity.class);
+            startActivity(i);
+        }
+
+
 
     }
 }
